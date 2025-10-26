aecdc3665525a10dc1f51582616682ae04e15e79
Hendrik Göbel <hendrik.goebel@cn-consult.eu>
2025-10-26 09:29:11 +0100

    - add qdrant client - add route to create hybrid collection

--- indexer/src/main/java/com/flenski/controller/IndexController.java
+++ indexer/src/main/java/com/flenski/controller/IndexController.java
@@ -6,7 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,18 +20,25 @@ import com.flenski.service.IndexerService;
 import com.flenski.service.PdfConverterService;
 import com.flenski.service.QueueService;
 
+import io.qdrant.client.QdrantClient;
+import io.qdrant.client.grpc.Collections.Distance;
+import io.qdrant.client.grpc.Collections.VectorParams;
+
 @RestController
 @RequestMapping("/api")
 public class IndexController {
+
     private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
     private final IndexerService indexerService;
     private final PdfConverterService pdfConverterService;
     private final QueueService queueService;
+    private final QdrantClient qdrantClient;
 
-    public IndexController(IndexerService indexerService, PdfConverterService pdfConverterService, QueueService queueService) {
+    public IndexController(IndexerService indexerService, PdfConverterService pdfConverterService, QueueService queueService, QdrantClient qdrantClient) {
         this.indexerService = indexerService;
         this.pdfConverterService = pdfConverterService;
         this.queueService = queueService;
+        this.qdrantClient = qdrantClient;
     }
 
     @PostMapping(value = "/queue", consumes = "application/json")
@@ -48,8 +57,8 @@ public class IndexController {
         return ResponseEntity.ok("No records received for queuing");
     }
 
-    @GetMapping(value = "/index")
-    public ResponseEntity<String> index() {
+    @GetMapping(value = "/index1")
+    public ResponseEntity<String> index1() {
         logger.info("Received GET request to /api/index");
 
         List<QueueItem> queueItems = queueService.getNext(10);
@@ -66,10 +75,29 @@ public class IndexController {
                 } catch (Exception e) {
                     logger.error("Error processing record: {}", record.getSourceUrl(), e);
                 }
-            };
+            }
         }
 
         String response = "Indexing completed";
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping(value = "/collection/{name}")
+    public ResponseEntity<String> hybrid(@PathVariable("name") String collectionName) {
+        logger.info("Received PUT request to /api/collection/{}", collectionName);
+
+        try {
+            this.qdrantClient.createCollectionAsync(collectionName,
+                    VectorParams.newBuilder()
+                            .setDistance(Distance.Cosine)
+                            .setSize(4)
+                            .build())
+                    .get();
+        } catch (Exception e) {
+            logger.error("Error creating collection: {}", e.getMessage(), e);
+        }
+
+        String response = String.format("Collection '%s' created", collectionName);
         return ResponseEntity.ok(response);
     }
 }
